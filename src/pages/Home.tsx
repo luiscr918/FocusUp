46b3750d66d6531c4bd4e28926ea700fa378de68
chasi <christopherchasiloa56@gmail.com>
2025-05-06 09:52:38 -0500

    prueba criko

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,7 @@ const Home = () => {
   return (
     <div>
       <h1>hola</h1>
+      <h1><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto eos illum reprehenderit exercitationem earum voluptates obcaecati doloremque quod, corrupti ipsa sint est repellat, voluptatibus, eius iste. Ab modi ullam ducimus.</p></h1>
     </div>
   )
 }
