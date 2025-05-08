dafd461aefc88afe8e7f1b570956e22a7c0c263b
andresfalcon20 <andresfalcon.18@hotmail.com>
2025-05-08 09:33:35 -0500

    a3

---
+++ src/components/ModalHomeComponent.tsx
@@ -0,0 +1,6 @@
+
+export const ModalHomeComponent = () => {
+  return (
+    <div>ModalHomeComponent</div>
+  )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,31 @@
+import { useState } from "react";
+
+
 
 const Home = () => {
+  const [modalA, setModalA] = useState(false);
+
+  const cambiarEstado = () => {
+    setModalA(!modalA);
+  };
+
+
   return (
     <>
- <div className="h-screen flex justify-center items-center">
-      <button className="bg-blue-500 text-white py-3 px-5 shadow-md ">
-        SELECCIONA
-      </button>
-    </div>
+      <div className="h-screen flex justify-center items-center">
+        <button
+          className="bg-blue-500 text-white py-3 px-5 shadow-md"
+        >
+
+          SELECCIONA
+        </button>
+
+        
+      </div>
+
 
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
