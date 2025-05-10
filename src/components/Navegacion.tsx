29d9c0560483c1d8c5d9334df50c70446d9405d2
alissonnoemi <alissonnoemiguapulemasandoval@gmail.com>
2025-05-10 15:49:06 -0500

    aaaa

--- src/components/Navegacion.tsx
+++ src/components/Navegacion.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 
-interface NavItem {
+interface Nav {
     name: string;
     link: string;
 }
 
-const navItems: NavItem[] = [
+const navV: Nav[] = [
     { name: 'Inicio', link: '#' },
     { name: 'Sobre Nosotros', link: '#' }
 ];
 
-const techniques: NavItem[] = [
+const tecnicas: Nav[] = [
     { name: 'Pomodoro', link: '#' },
     { name: 'Técnica Feynman', link: '#' },
-    { name: 'Mapas Mentales', link: '#' }
+
 ];
 
 const Navegacion: React.FC = () => {
@@ -22,7 +22,7 @@ const Navegacion: React.FC = () => {
             <div className="container mx-auto flex justify-between items-center">
                 <div className="text-2xl font-bold">FocusUp</div>
                 <ul className="flex space-x-6">
-                    {navItems.map((item) => (
+                    {navV.map((item) => (
                         <li key={item.name} className="hover:text-gray-300">
                             <a href={item.link}>{item.name}</a>
                         </li>
@@ -32,7 +32,7 @@ const Navegacion: React.FC = () => {
                             Técnicas
                         </button>
                         <ul className="absolute hidden group-hover:block bg-white text-black p-2 shadow-lg rounded mt-2 w-40">
-                            {techniques.map((tech) => (
+                            {tecnicas.map((tech) => (
                                 <li key={tech.name} className="p-1 hover:bg-gray-200 rounded-md">
                                     <a href={tech.link}>{tech.name}</a>
                                 </li>
@@ -41,6 +41,7 @@ const Navegacion: React.FC = () => {
                     </li>
                 </ul>
             </div>
+
         </nav>
     );
 };
