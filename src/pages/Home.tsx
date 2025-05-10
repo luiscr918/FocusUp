1230cf33e1f14471e1af05bf34f6219daef4eb26
luiscr918 <lc9107040@gmail.com>
2025-05-10 10:46:19 -0500

    imagen de logo agregado nombre de pagina y toogle component primera version

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,8 @@
 import { NightSky } from "../components/NightSky";
 import "../styles/fondosMain.css";
 import { useState } from "react";
+import { ToogleComponent } from '../components/ToogleComponent';
+
 
 
 export const Home = () => {
@@ -13,7 +15,7 @@ export const Home = () => {
       {noche && <NightSky />}
       <p style={{ color: 'white' }}>hola</p>
       <button type="button" onClick={() => setNoche(!noche)} className="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900">Yellow</button>
-
+      <ToogleComponent />
 
 
 
