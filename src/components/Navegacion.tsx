f34fd9aec1d70fc9dda81d127809eac0c3ccbef0
luiscr918 <lc9107040@gmail.com>
2025-05-10 20:17:04 -0500

    barra navegacion modo noche

--- src/components/Navegacion.tsx
+++ src/components/Navegacion.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import { FaSpotify } from 'react-icons/fa'; // Importa el ícono de Spotify
 import { ToogleComponent } from './ToogleComponent';
 
@@ -27,19 +27,19 @@ export const Navegacion = ({ isChecked, setIsChecked }: Props) => {
     return (
         <nav className="p-4">
             <div className="container mx-auto flex items-center justify-between">
-                <div className="text-4xl font-bold text-black">FocusUp</div>
+                <div className={`text-4xl font-bold ${isChecked ? 'text-white' : 'text-black'} `}>FocusUp</div>
                 <div className="flex justify-center flex-grow">
                     <ul className="flex space-x-6">
                         {/* Inicio */}
                         {navV.map((item) => (
                             <li key={item.name} className="hover:text-gray-700">
-                                <a className="text-black" href={item.link}>{item.name}</a>
+                                <a className={`${isChecked ? 'text-white' : 'text-black'}`} href={item.link}>{item.name}</a>
                             </li>
                         ))}
 
                         {/* Técnicas */}
                         <li className="relative group">
-                            <button className="flex items-center text-black hover:text-gray-700">
+                            <button className={`flex items-center ${isChecked ? 'text-white' : 'text-black'} hover:text-gray-700`}>
                                 Técnicas
                                 <svg className="ml-1 w-4 h-4 fill-current" viewBox="0 0 20 20">
                                     <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
@@ -54,7 +54,7 @@ export const Navegacion = ({ isChecked, setIsChecked }: Props) => {
                             </ul>
                         </li>
                         {/* Ícono de Spotify */}
-                        <div className="text-black text-2xl">
+                        <div className={`${isChecked ? 'text-white' : 'text-black'} text-2xl`}>
                             <a href="https://spotify.com" target="_blank" rel="noopener noreferrer">
                                 <FaSpotify className="hover:text-green-500 cursor-pointer" />
                             </a>
