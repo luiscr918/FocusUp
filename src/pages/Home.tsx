124817f10fbe641bcc28d530e2f7bde80355fcfa
luiscr918 <lc9107040@gmail.com>
2025-05-12 22:35:18 -0500

    heading arreglado colores

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -27,14 +27,11 @@ export const Home = () => {
       {/*Heading Principal */}
       <div className="container mx-auto flex flex-col items-center justify-center h-screen text-center ">
         <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">MEJORA TU ENFOQUE EN EL ESTUDIO</h1>
-        <p className="mb-6 text-lg font-normal text-gray-200 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-200">Organiza tu tiempo, potencia tu aprendizaje y alcanza tus metas con métodos probados. ¿Listo para concentrarte de verdad?</p>
+        <p className={`mb-6 text-lg font-normal  lg:text-xl sm:px-16 xl:px-48 ${noche ? 'text-teal-200' : 'text-gray-800'}`}>Organiza tu tiempo, potencia tu aprendizaje y alcanza tus metas con métodos probados. ¿Listo para concentrarte de verdad?</p>
         <button
           onClick={abrirModal}
-          className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+          className={`inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white  rounded-lg  focus:ring-4 ${noche ? 'bg-cyan-400 hover:bg-cyan-500' : 'bg-teal-400 hover:bg-teal-500'} `}>
           Comenzar Ahora
-          <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-          </svg>
         </button>
         <img src="/src/assets/imgs/logoSinFondo.png" alt="icono de FocusUp" />
       </div>
