dd5aefaf1feee65356addd7c9e5de4e8311597de
Jean <jeanzeykoharcode@gmail.com>
2025-05-14 18:56:00 -0500

    listo2.0

--- src/pages/Aboutus.tsx
+++ src/pages/Aboutus.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Navegacion } from "../components/Navegacion";
 import { NightSky } from "../components/NightSky";
 import Footer from "../components/Footer";
+import { Link } from "react-router-dom";
 
 export const Aboutus = () => {
     const [noche, setNoche] = useState(false);
@@ -11,24 +12,23 @@ export const Aboutus = () => {
             {/* Barra de navegacion */}
             <Navegacion isChecked={noche} setIsChecked={setNoche} />
             {/* contenido */}
-            <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
-                <img src="/src/assets/imgs/logoSinFondo.png" alt="imagen_logo" className="absolute inset-0 -z-450 size-full object-cover object-center md:object-center" />
-                <div className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl" aria-hidden="true">
-                    <div className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20" style={{ clipPath: "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" }}></div>
-                </div>
-                <div className="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu" aria-hidden="true">
-                    <div className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20" style={{ clipPath: "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" }}></div>
-                </div>
-                <div className="mx-auto max-w-7xl px-6 lg:px-8">
-                    <div className="mx-auto max-w-2xl lg:mx-0">
-                        <h2 className="text-5xl font-semibold tracking-tight text-white sm:text-7xl">Conoce más de Nosotros</h2>
-                        <p className="mt-8 text-pretty text-lg font-medium text-white sm:text-xl/8">En nuestra galería de arte, somos un equipo apasionado por el mundo de la creatividad y la expresión artística. Cada uno de nosotros comparte el objetivo común de brindar a nuestros clientes una experiencia única, conectándolos con obras que inspiran, transforman y cuentan historias.
-                            Desde los curadores que seleccionan cuidadosamente cada cuadro hasta los asesores que te guiarán con calidez y profesionalismo, trabajamos juntos para ofrecer una colección diversa que incluye desde arte clásico hasta piezas contemporáneas. Nuestra misión es crear un espacio donde el talento de artistas emergentes y consagrados pueda ser valorado y apreciado.
-                            Creemos que el arte tiene el poder de enriquecer la vida, y estamos aquí para ayudarte a descubrir la pieza perfecta que resuene contigo. Porque detrás de cada obra, hay no solo un artista, sino un equipo comprometido con el amor por el arte.</p>
+                <section className="py-24 relative">
+                    <div className="w-full max-w-7xl px-4 md:px-5 lg:px-5 mx-auto">
+                    <div className="w-full justify-start items-center gap-8 grid lg:grid-cols-2 grid-cols-1">
+                <div className="w-full flex-col justify-start lg:items-start items-center gap-10 inline-flex">
+                    <div className="w-full flex-col justify-start lg:items-start items-center gap-4 flex">
+                        <h2 className="text-white text-4xl font-bold font-manrope leading-normal lg:text-start text-center">¿Quienes Somos?</h2>
+                        <p className="text-white text-base font-normal leading-relaxed lg:text-start text-justify">FocusUp es un gestor de estudio diseñado para optimizar el aprendizaje combinando metodologías efectivas como Pomodoro, Feynman, el método Cornell y mapas mentales. Esta herramienta ayuda a los estudiantes a organizar su tiempo y contenido de forma eficiente: con Pomodoro mejora la concentración mediante sesiones temporizadas; el método Feynman promueve la comprensión profunda al explicar conceptos con palabras simples; Cornell facilita la toma de apuntes estructurada;
+                            y los mapas mentales permiten visualizar y conectar ideas de manera clara. FocusUp integra estas técnicas en una sola plataforma para potenciar el rendimiento académico.</p>
                     </div>
-                    <button>
-                        IR AL INICIO
+                    <button className="sm:w-fit w-full px-3.5 py-2 bg-teal-400 hover:bg-teal-500 transition-all duration-700 ease-in-out rounded-lg shadow-[0px_1px_2px_0px_rgba(16,_24,_40,_0.05)] justify-center items-center flex">
+                        <Link to="/home">Ir al Inicio</Link>
                     </button>
+                </div>
+                <img className="lg:mx-0 mx-auto h-full rounded-3xl object-cover" src="/src/assets/imgs/logoSinFondo.png" alt="about Us image" />
+            </div>
+        </div>
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
                     <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
                         <dl className="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
                             <div className="flex flex-col-reverse gap-1">
@@ -50,17 +50,17 @@ export const Aboutus = () => {
                         </dl>
                     </div>
                 </div>
-            </div>
+    </section>                  
+            
             <div className={`py-24 sm:py-32 ${noche ? 'cuerpo_noche' : 'cielo_animado'}`}>
-                <div className="mx-auto grid max-w-7xl gap-20 px-6 lg:px-8 xl:grid-cols-3">
+                <div className="mx-auto grid max-w-6xl gap-10 px-6 lg:px-2 xl:grid-cols-3">
                     <div className="max-w-xl">
-                        <h2 className="text-pretty text-3xl font-semibold tracking-tight text-white sm:text-4xl">Conoce a nuestro Grupo de trabajo</h2>
+                        <h2 className="text-pretty text-3xl font-semibold tracking-tight text-white sm:text-4xl">Conoce a Nuestro Grupo de Trabajo</h2>
                         <p className="mt-6 text-lg/8 text-white">We’re a dynamic group of individuals who are passionate about what we do and dedicated to delivering the best results for our clients.</p>
                     </div>
-                    <ul role="list" className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
+                    <ul role="list" className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-10 xl:col-span-2">
                         <li>
                             <div className="flex items-center gap-x-6">
-                                <img className="size-16 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
                                 <div>
                                     <h3 className="text-base/7 font-semibold tracking-tight text-white">Leslie Alexander</h3>
                                     <p className="text-sm/6 font-semibold text-white">Co-Founder / CEO</p>
@@ -69,7 +69,6 @@ export const Aboutus = () => {
                         </li>
                         <li>
                             <div className="flex items-center gap-x-6">
-                                <img className="size-16 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
                                 <div>
                                     <h3 className="text-base/7 font-semibold tracking-tight text-white">Leslie Alexander</h3>
                                     <p className="text-sm/6 font-semibold text-white">Co-Founder / CEO</p>
@@ -78,7 +77,6 @@ export const Aboutus = () => {
                         </li>
                         <li>
                             <div className="flex items-center gap-x-6">
-                                <img className="size-16 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
                                 <div>
                                     <h3 className="text-base/7 font-semibold tracking-tight text-white">Leslie Alexander</h3>
                                     <p className="text-sm/6 font-semibold text-white">Co-Founder / CEO</p>
@@ -87,7 +85,6 @@ export const Aboutus = () => {
                         </li>
                         <li>
                             <div className="flex items-center gap-x-6">
-                                <img className="size-16 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
                                 <div>
                                     <h3 className="text-base/7 font-semibold tracking-tight text-white">Leslie Alexander</h3>
                                     <p className="text-sm/6 font-semibold text-white">Co-Founder / CEO</p>
@@ -96,7 +93,6 @@ export const Aboutus = () => {
                         </li>
                         <li>
                             <div className="flex items-center gap-x-6">
-                                <img className="size-16 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
                                 <div>
                                     <h3 className="text-base/7 font-semibold tracking-tight text-white">Leslie Alexander</h3>
                                     <p className="text-sm/6 font-semibold text-white">Co-Founder / CEO</p>
@@ -105,7 +101,6 @@ export const Aboutus = () => {
                         </li>
                         <li>
                             <div className="flex items-center gap-x-6">
-                                <img className="size-16 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
                                 <div>
                                     <h3 className="text-base/7 font-semibold tracking-tight text-white">Leslie Alexander</h3>
                                     <p className="text-sm/6 font-semibold text-white">Co-Founder / CEO</p>
