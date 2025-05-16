399c3b263f4bc05800e833757a59a7311e88825e
andresfalcon20 <andresfalcon.18@hotmail.com>
2025-05-15 20:59:55 -0500

    inputdiseñook

--- src/components/ModalTarea.tsx
+++ src/components/ModalTarea.tsx
@@ -1,32 +1,35 @@
-
 interface Props {
     closeModalTarea: () => void;
     abrirPomodoro: () => void;
+    
 }
 
 export const ModalTarea = ({ closeModalTarea, abrirPomodoro }: Props) => {
     return (
         <div className="fixed inset-0 flex justify-center items-center backdrop-blur-xl z-50">
-            <div className={`rounded-lg w-125 p-6 shadow-md border-2 border-white`}>
-                <h2 className="text-xl font-semibold text-center">INGRESE SU TAREA</h2>
-                <input 
-    className="bg-white text-black border border-gray-300 rounded-md p-2 outline-none focus:border-blue-500 " 
-    type="text" 
-    placeholder="Escribe tu tarea aquí..."
-/>
-
-                <div className="flex gap-4 mt-4">
-                    <button 
-                        onClick={closeModalTarea} 
-                        className="text-red-500 hover:text-red-600"
-                    >
-                        Cerrar
+            <div className="relative rounded-lg w-125 p-6 shadow-md border-2 border-white flex flex-col items-center">
+                
+                <div className="w-full flex justify-between items-center mb-1">
+                    <h2 className="text-xl font-semibold text-center flex-grow">INGRESE SU TAREA</h2>
+                    <button onClick={closeModalTarea} className="bg-red-500 px-3 py-1 rounded">
+                        X
                     </button>
+                </div>
+
+                <div className="border-b-2 border-white my-4 w-full"></div>
+
+                <input 
+                    className="bg-white text-black border border-gray-100 rounded-md p-3 focus:border-blue-500 w-70 text-center" 
+                    type="text" 
+                    placeholder="Escribe tu tarea aquí..." 
+                />
+
+                <div className="flex gap-4 mt-6">
                     <button 
                         onClick={() => { closeModalTarea(); abrirPomodoro(); }} 
-                        className="text-blue-500 hover:text-blue-600"
+                        className="text-white hover:text-blue-600"
                     >
-                        Iniciar Pomodoro
+                        INICIAR POMODORO
                     </button>
                 </div>
             </div>
