5431e715441a18c7ab56198e518555da02996021
luiscr918 <lc9107040@gmail.com>
2025-05-16 14:30:02 -0500

    primera version ingr tarea

--- src/pages/Pomodoro.tsx
+++ src/pages/Pomodoro.tsx
@@ -4,19 +4,27 @@ import { NightSky } from "../components/NightSky";
 import "../styles/fondosMain.css";
 import Footer from "../components/Footer";
 import { TempPomodoro } from "../components/TempPomodoro";
+import { useNavigate } from "react-router-dom";
+import { ModalTarea } from "../components/ModalTarea";
 interface Props {
     tarea?: string;
 }
 
-export const Pomodoro = ({ tarea = 'jfjfj' }: Props) => {
+export const Pomodoro = ({ tarea = '' }: Props) => {
     const [noche, setNoche] = useState(false);
+    const navigation = useNavigate();
+    const [nombre, setNombre] = useState(tarea);
+    const volverHome = () => {
+        navigation('/');
+    }
+
     return (
         <div className={`${noche ? 'cuerpo_noche' : 'cielo_animado'}`}>
             {noche && <NightSky />}
             {/* Barra de navegacion */}
             <Navegacion isChecked={noche} setIsChecked={setNoche} />
-            {(tarea === '')
-                ? (<p>hola</p>)
+            {(nombre === '')
+                ? (<ModalTarea closeModalTarea={volverHome} guardarNombre={setNombre}/>)
                 : (<TempPomodoro />)
             }
             <Footer isChecked={noche} />
