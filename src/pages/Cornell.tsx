9c4e99115d46949dbcc54911eb2d57b9531b203b
luiscr918 <lc9107040@gmail.com>
2025-05-20 18:40:00 -0500

    diesño cornell listo noche

--- src/pages/Cornell.tsx
+++ src/pages/Cornell.tsx
@@ -26,23 +26,25 @@ export const Cornell = () => {
         {/* TITULO */}
         <label
           htmlFor="titulo"
-          className={`block mb-2 text-sm font-medium ${noche ? 'text-teal-200':'text-gray-800'}`}
+          className={`block mb-2 text-sm font-medium ${noche ? 'text-teal-200' : 'text-gray-800'}`}
         >
           TITULO
         </label>
         <textarea
           id="titulo"
           ref={tituloRef}
-          rows={1}
+          rows={2}
           onInput={() => handleInput(tituloRef)}
-          className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 resize-none"
+          className={`block p-2.5 w-full text-sm   rounded-lg border resize-none
+            ${noche ? 'bg-gray-600 text-white placeholder-gray-50 border-gray-100 ' : 'bg-gray-50 text-gray-900 border-gray-300 '}
+            `}
           placeholder="Escribe el título de la lección o tema"
         ></textarea>
         {/* IDEAS PRINCIPALES Y NOTAS DE CLASE */}
         <div className="flex flex-col md:flex-row gap-4 mt-4">
           {/* IDEAS PRINCIPALES */}
           <div className="w-full md:w-1/2">
-            <label htmlFor="ideas-principales" className={`block mb-2 text-sm font-medium ${noche ? 'text-teal-200':'text-gray-800'}`}>
+            <label htmlFor="ideas-principales" className={`block mb-2 text-sm font-medium ${noche ? 'text-teal-200' : 'text-gray-800'}`}>
               IDEAS PRINCIPALES
             </label>
             <textarea
@@ -50,13 +52,15 @@ export const Cornell = () => {
               ref={ideasRef}
               rows={10}
               onInput={() => handleInput(ideasRef)}
-              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 resize-none"
+              className={`block p-2.5 w-full text-sm   rounded-lg border resize-none
+            ${noche ? 'bg-gray-600 text-white placeholder-gray-50 border-gray-100 ' : 'bg-gray-50 text-gray-900 border-gray-300 '}
+            `}
               placeholder="Anota las ideas clave o conceptos principales"
             ></textarea>
           </div>
           {/* NOTAS DE CLASE */}
           <div className="w-full md:w-1/2">
-            <label htmlFor="notas-clase" className={`block mb-2 text-sm font-medium ${noche ? 'text-teal-200':'text-gray-800'}`}>
+            <label htmlFor="notas-clase" className={`block mb-2 text-sm font-medium ${noche ? 'text-teal-200' : 'text-gray-800'}`}>
               NOTAS DE CLASE
             </label>
             <textarea
@@ -64,7 +68,9 @@ export const Cornell = () => {
               ref={notasRef}
               rows={10}
               onInput={() => handleInput(notasRef)}
-              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 resize-none"
+              className={`block p-2.5 w-full text-sm   rounded-lg border resize-none
+            ${noche ? 'bg-gray-600 text-white placeholder-gray-50 border-gray-100 ' : 'bg-gray-50 text-gray-900 border-gray-300 '}
+            `}
               placeholder="Agrega tus notas o detalles importantes"
             ></textarea>
           </div>
@@ -72,7 +78,7 @@ export const Cornell = () => {
         {/* RESUMEN */}
         <label
           htmlFor="resumen"
-          className={`block mb-2 text-sm font-medium ${noche ? 'text-teal-200':'text-gray-800'}`}
+          className={`block mb-2 text-sm font-medium ${noche ? 'text-teal-200' : 'text-gray-800'}`}
         >
           RESUMEN
         </label>
@@ -81,16 +87,18 @@ export const Cornell = () => {
           ref={resumenRef}
           rows={6}
           onInput={() => handleInput(resumenRef)}
-          className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 resize-none"
+          className={`block p-2.5 w-full text-sm   rounded-lg border resize-none
+            ${noche ? 'bg-gray-600 text-white placeholder-gray-50 border-gray-100 ' : 'bg-gray-50 text-gray-900 border-gray-300 '}
+            `}
           placeholder="Escribe un resumen breve de las ideas principales aquí..."
         ></textarea>
       </form>
       <div className="flex justify-center mt-4">
         <button type="button" className={`${noche
-          ?'text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600   focus:ring-teal-300  shadow-lg shadow-teal-500/50   '
-          :'text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600   focus:ring-cyan-300  shadow-lg shadow-cyan-500/50  '
-        } hover:bg-gradient-to-br focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2`}
-        
+          ? 'text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600   focus:ring-teal-300  shadow-lg shadow-teal-500/50   '
+          : 'text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600   focus:ring-cyan-300  shadow-lg shadow-cyan-500/50  '
+          } hover:bg-gradient-to-br focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2`}
+
         >DESCARGAR COMO PDF</button>
       </div>
       <Footer isChecked={noche} />
