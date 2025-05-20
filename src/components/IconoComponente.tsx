d7dff483fd1c85754cd0cd7e2fba76d104d29bbb
jeanitaz <jeanzeykoharcode@gmail.com>
2025-05-20 10:35:27 -0500

    estatico

--- src/components/IconoComponente.tsx
+++ src/components/IconoComponente.tsx
@@ -7,7 +7,7 @@ interface Props{
 
 export const IconoComponent = ({noche}:Props) => {
     return (
-        <div className="fixed top right-4 z-50 text-white">
+        <div className="absolute right-4 z-50 text-white">
             <button
                 rel="noopener noreferrer"
                 className={`w-20 h-20 sm:w-24 sm:h-24 md:w-28 md:h-28  flex items-center justify-center ${noche ? 'hover:text-teal-200' : 'hover:text-yellow-400'}`}>
