e84b552c00cbd14a856c2be9deb2c3b2e8af0dd0
luis <lc9107040@gmail.com>
2025-05-21 07:45:50 -0500

    validación de campos cornell completo

--- src/pages/Cornell.tsx
+++ src/pages/Cornell.tsx
@@ -3,9 +3,20 @@ import { Navegacion } from "../components/Navegacion";
 import { NightSky } from "../components/NightSky";
 import Footer from "../components/Footer";
 import { useReactToPrint } from "react-to-print";
-
+interface Campos {
+  titulo: string;
+  ideasPrincipales: string;
+  notasClase: string;
+  resumen: string;
+}
 export const Cornell = () => {
   const [noche, setNoche] = useState(false);
+  const [formulario, setFormulario] = useState<Campos>({
+    titulo: "",
+    ideasPrincipales: "",
+    notasClase: "",
+    resumen: "",
+  });
   const tituloRef = useRef<HTMLTextAreaElement | null>(null);
   const ideasRef = useRef<HTMLTextAreaElement | null>(null);
   const notasRef = useRef<HTMLTextAreaElement | null>(null);
@@ -19,10 +30,21 @@ export const Cornell = () => {
       textarea.style.height = textarea.scrollHeight + "px";
     }
   };
-
+  const handleDescargar = () => {
+    if (
+      !formulario.titulo.trim() ||
+      !formulario.ideasPrincipales.trim() ||
+      !formulario.notasClase.trim() ||
+      !formulario.resumen.trim()
+    ) {
+      alert("Por favor, completa todos los campos antes de descargar.");
+      return;
+    }
+    handlePrint();
+  };
   const handlePrint = useReactToPrint({
     contentRef: formRef,
-    documentTitle: 'Notas Cornell',
+    documentTitle: "Notas Cornell",
   });
 
   return (
@@ -33,7 +55,9 @@ export const Cornell = () => {
         {/* TITULO */}
         <label
           htmlFor="titulo"
-          className={`block mb-2 text-sm font-medium ${noche ? 'text-teal-400' : 'text-gray-800'}`}
+          className={`block mb-2 text-sm font-medium ${
+            noche ? "text-teal-400" : "text-gray-800"
+          }`}
         >
           TITULO
         </label>
@@ -43,8 +67,16 @@ export const Cornell = () => {
           rows={2}
           onInput={() => handleInput(tituloRef)}
           className={`block p-2.5 w-full text-sm rounded-lg border resize-none
-            ${noche ? 'bg-gray-600 text-white placeholder-gray-50 border-gray-100' : 'bg-gray-50 text-gray-900 border-gray-300'}
+            ${
+              noche
+                ? "bg-gray-600 text-white placeholder-gray-50 border-gray-100"
+                : "bg-gray-50 text-gray-900 border-gray-300"
+            }
           `}
+          value={formulario.titulo}
+          onChange={(e) =>
+            setFormulario({ ...formulario, titulo: e.target.value })
+          }
           placeholder="Escribe el título de la lección o tema"
         ></textarea>
 
@@ -52,7 +84,12 @@ export const Cornell = () => {
         <div className="flex flex-col md:flex-row gap-4 mt-4">
           {/* IDEAS PRINCIPALES */}
           <div className="w-full md:w-1/2">
-            <label htmlFor="ideas-principales" className={`block mb-2 text-sm font-medium ${noche ? 'text-teal-400' : 'text-gray-800'}`}>
+            <label
+              htmlFor="ideas-principales"
+              className={`block mb-2 text-sm font-medium ${
+                noche ? "text-teal-400" : "text-gray-800"
+              }`}
+            >
               IDEAS PRINCIPALES
             </label>
             <textarea
@@ -61,15 +98,31 @@ export const Cornell = () => {
               rows={10}
               onInput={() => handleInput(ideasRef)}
               className={`block p-2.5 w-full text-sm rounded-lg border resize-none
-                ${noche ? 'bg-gray-600 text-white placeholder-gray-50 border-gray-100' : 'bg-gray-50 text-gray-900 border-gray-300'}
+                ${
+                  noche
+                    ? "bg-gray-600 text-white placeholder-gray-50 border-gray-100"
+                    : "bg-gray-50 text-gray-900 border-gray-300"
+                }
               `}
+              value={formulario.ideasPrincipales}
+              onChange={(e) =>
+                setFormulario({
+                  ...formulario,
+                  ideasPrincipales: e.target.value,
+                })
+              }
               placeholder="Anota las ideas clave o conceptos principales"
             ></textarea>
           </div>
 
           {/* NOTAS DE CLASE */}
           <div className="w-full md:w-1/2">
-            <label htmlFor="notas-clase" className={`block mb-2 text-sm font-medium ${noche ? 'text-teal-400' : 'text-gray-800'}`}>
+            <label
+              htmlFor="notas-clase"
+              className={`block mb-2 text-sm font-medium ${
+                noche ? "text-teal-400" : "text-gray-800"
+              }`}
+            >
               NOTAS DE CLASE
             </label>
             <textarea
@@ -78,8 +131,16 @@ export const Cornell = () => {
               rows={10}
               onInput={() => handleInput(notasRef)}
               className={`block p-2.5 w-full text-sm rounded-lg border resize-none
-                ${noche ? 'bg-gray-600 text-white placeholder-gray-50 border-gray-100' : 'bg-gray-50 text-gray-900 border-gray-300'}
+                ${
+                  noche
+                    ? "bg-gray-600 text-white placeholder-gray-50 border-gray-100"
+                    : "bg-gray-50 text-gray-900 border-gray-300"
+                }
               `}
+              value={formulario.notasClase}
+              onChange={(e) =>
+                setFormulario({ ...formulario, notasClase: e.target.value })
+              }
               placeholder="Agrega tus notas o detalles importantes"
             ></textarea>
           </div>
@@ -88,7 +149,9 @@ export const Cornell = () => {
         {/* RESUMEN */}
         <label
           htmlFor="resumen"
-          className={`block mb-2 text-sm font-medium ${noche ? 'text-teal-400' : 'text-gray-800'}`}
+          className={`block mb-2 text-sm font-medium ${
+            noche ? "text-teal-400" : "text-gray-800"
+          }`}
         >
           RESUMEN
         </label>
@@ -98,8 +161,16 @@ export const Cornell = () => {
           rows={6}
           onInput={() => handleInput(resumenRef)}
           className={`block p-2.5 w-full text-sm rounded-lg border resize-none
-            ${noche ? 'bg-gray-600 text-white placeholder-gray-50 border-gray-100' : 'bg-gray-50 text-gray-900 border-gray-300'}
+            ${
+              noche
+                ? "bg-gray-600 text-white placeholder-gray-50 border-gray-100"
+                : "bg-gray-50 text-gray-900 border-gray-300"
+            }
           `}
+          value={formulario.resumen}
+          onChange={(e) =>
+            setFormulario({ ...formulario, resumen: e.target.value })
+          }
           placeholder="Escribe un resumen breve de las ideas principales aquí..."
         ></textarea>
       </form>
@@ -107,11 +178,12 @@ export const Cornell = () => {
       <div className="flex justify-center mt-4">
         <button
           type="button"
-          onClick={handlePrint}
-          className={`${noche
-            ? 'text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 focus:ring-teal-300 shadow-lg shadow-teal-500/50'
-            : 'text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 focus:ring-cyan-300 shadow-lg shadow-cyan-500/50'
-            } hover:bg-gradient-to-br focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2`}
+          onClick={handleDescargar}
+          className={`${
+            noche
+              ? "text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 focus:ring-teal-300 shadow-lg shadow-teal-500/50"
+              : "text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 focus:ring-cyan-300 shadow-lg shadow-cyan-500/50"
+          } hover:bg-gradient-to-br focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2`}
         >
           DESCARGAR COMO PDF
         </button>
@@ -120,4 +192,3 @@ export const Cornell = () => {
     </div>
   );
 };
-
