8b4f37135c137fe9a3f9f77c75824e7a4171fbba
luiscr918 <lc9107040@gmail.com>
2025-05-22 20:35:19 -0500

    footer reducido

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,6 @@
 
 
-const defaultFooterLinks = [
-    { label: 'Características', url: '#' },
-    { label: 'Ayuda', url: '#' },
-    { label: 'Política de privacidad', url: '#' },
-];
+
 
 interface FooterProps {
     isChecked: boolean; // Prop para controlar el estado del tema
@@ -17,27 +13,10 @@ const Footer = ({ isChecked }: FooterProps) => {
     return (
         <footer className="py-5 w-full z-10">
             <div className="container mx-auto flex flex-col items-center justify-center">
-                <ul className="flex gap-5 mb-3">
-                    {defaultFooterLinks.map((link) => (
-                        <li key={link.label}>
-                            <a
-                                className={`transition-colors ${
-                                    isChecked
-                                        ? 'text-white hover:text-black'
-                                        : 'text-black hover:text-gray-700'
-                                }`}
-                                href={link.url}
-                                aria-label={link.label}
-                            >
-                                {link.label}
-                            </a>
-                        </li>
-                    ))}
-                </ul>
+
                 <p
-                    className={`text-sm transition-colors ${
-                        isChecked ? 'text-white' : 'text-black'
-                    }`}
+                    className={`text-sm transition-colors ${isChecked ? 'text-white' : 'text-black'
+                        }`}
                 >
                     © {currentYear} {companyName}. Todos los derechos reservados.
                 </p>
