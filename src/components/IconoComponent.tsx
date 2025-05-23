60941a30c6f074c4a1016fc76bc33c531309819c
luiscr918 <lc9107040@gmail.com>
2025-05-22 21:33:42 -0500

    icono fijo de guia implementado

--- src/components/IconoComponent.tsx
+++ src/components/IconoComponent.tsx
@@ -2,25 +2,25 @@
 import { FiHelpCircle } from "react-icons/fi";
 import { useNavigate } from "react-router-dom";
 
-interface Props{
+interface Props {
     noche: boolean
-    nombre:string
+    nombre: string
 }
 
-export const IconoComponent = ({noche, nombre}:Props) => {
+export const IconoComponent = ({ noche, nombre }: Props) => {
 
-const navigate=useNavigate();
-const navegarGuia=()=>{
-    navigate(`/guia/${nombre}`);
-}
+    const navigate = useNavigate();
+    const navegarGuia = () => {
+        navigate(`/guia/${nombre}`);
+    }
     return (
-        <div className="fixed top right-4 z-50 text-white">
+        <div className="absolute top-10 right-2 z-50 text-white">
             <button
-            type="button"
+                type="button"
                 onClick={navegarGuia}
                 rel="noopener noreferrer"
                 className={`w-20 h-20 sm:w-24 sm:h-24 md:w-28 md:h-28  flex items-center justify-center ${noche ? 'hover:text-teal-200' : 'hover:text-yellow-400'}`}>
-                    <FiHelpCircle className="md:text-5xl cursor-pointer" />
+                <FiHelpCircle className="md:text-5xl cursor-pointer" />
             </button>
         </div>
 
