e2defb6cc6b526c4f578ab90e29db6d273fc0c82
luiscr918 <lc9107040@gmail.com>
2025-05-25 21:05:35 -0500

    icono llevar a home arreglado

--- src/components/Navegacion.tsx
+++ src/components/Navegacion.tsx
@@ -35,8 +35,9 @@ export const Navegacion = ({ isChecked, setIsChecked }: Props) => {
         <nav className="p-4 text-white relative z-50">
             <div className="container mx-auto flex flex-wrap items-center justify-between">
                 {/* Logo */}
-                <div className="text-2xl font-bold flex-shrink-0">FocusUp</div>
-
+                <Link to={'/'}>
+                    <div className="text-2xl font-bold flex-shrink-0">FocusUp</div>
+                </Link>
                 {/* Botón hamburguesa */}
                 <button
                     className="md:hidden flex items-center px-3 py-2 border rounded text-white border-white"
